a0876292949080d5a7acdb8b11936de289c062a5
clauxbee <clauxgrl@gmail.com>
2022-04-07 11:58:20 -0500

    Diseño de pantalla de inicio de sesión

--- php_components/login_form.php
+++ php_components/login_form.php
@@ -3,33 +3,42 @@
         <div class="container">
             <div class="col-md-6">
                 <div class="login-page">
-                    <h4 class="title">New Customers</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis</p>
-                    <div class="button1">
-                        <a href="register.html"><input type="submit" name="Submit" value="Create an Account"></a>
+                <br> <br>
+                    <h4 class="title">Nuevos miembros</h4>
+                    <br>
+                    <p>Queremos lo mejor para tu peludito, es por ello que en "Huellitas aventureras" nos empeñamos todos los días en hacerlo sentir bien y cuidarlos como si fueran nuestros. Piensa en tu peludito y regalale un paseo con los mejores paseadores de la ciudad, forma parte de nuestra gran familia</p>
+                   
+                    <div class="btnn">
+                    <button href="register.html" type="getnow" name="subscribe" class="btn btn-primary btn-lg" required="required">Crear una cuenta</button>
                     </div>
                     <div class="clear"></div>
                 </div>
             </div>
             <div class="col-md-6">
                 <div class="login-title">
-                    <h4 class="title">Registered Customers</h4>
+                <br> <br>
+                    <h4 class="title">¿Ya tienes una cuenta? Inicia sesión</h4>
+                    
+                    <br>
                     <div id="loginbox" class="loginbox">
                         <form action="" method="post" name="login" id="login-form">
                             <fieldset class="input">
                                 <p id="login-form-username">
-                                    <label for="modlgn_username">Email</label>
+                                    <label for="modlgn_username">Correo</label>
                                     <input id="modlgn_username" type="text" name="email" class="inputbox" size="18" autocomplete="off">
                                 </p>
                                 <p id="login-form-password">
-                                    <label for="modlgn_passwd">Password</label>
+                                    <label for="modlgn_passwd">Contraseña</label>
                                     <input id="modlgn_passwd" type="password" name="password" class="inputbox" size="18" autocomplete="off">
                                 </p>
                                 <div class="remember">
                                     <p id="login-form-remember">
-                                        <label for="modlgn_remember"><a href="#">Forget Your Password ? </a></label>
+                                        <label for="modlgn_remember"><a href="#">¿Olvidaste tu contraseña? </a></label>
                                     </p>
-                                    <input type="submit" name="Submit" class="button" value="Login"><div class="clear"></div>
+                                    
+                                    <button href="inicio.html" type="getnow" name="subscribe" class="btn btn-primary btn-lg" required="required">Iniciar sesión</button>
+                                    <br><br><br><br>
+                               
                                 </div>
                             </fieldset>
                         </form>
