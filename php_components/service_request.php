e99e0a246a3e3a5469fbc27284ceae05a6181484
Braulio-Palagot <bdpalagot@gmail.com>
2022-04-07 14:51:32 -0500

    Update service_request.php
    Se actualizó la vista de solicitud de paseo para adecuarse a la aplicación en desarrollo.

--- php_components/service_request.php
+++ php_components/service_request.php
@@ -1,41 +1,41 @@
 <section class="contact-page">
     <div class="container">
         <div class="text-center">
-            <h2>Drop Your Message</h2>
-            <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <h2>Solicitud de Paseo</h2>
+            <p>Rellene los campos necesarios para solicitar un paseo para su mascota.</p>
         </div>
         <div class="row contact-wrap">
             <div class="status alert alert-success" style="display: none"></div>
             <form id="main-contact-form" class="contact-form" name="contact-form" method="post" action="sendemail.php">
                 <div class="col-sm-5 col-sm-offset-1">
                     <div class="form-group">
-                        <label>Name *</label>
-                        <input type="text" name="name" class="form-control" required="required">
+                        <label>Nombre de la Mascota *</label>
+                        <input type="text" name="petName" class="form-control" required="required" placeholder="Mascota">
                     </div>
                     <div class="form-group">
-                        <label>Email *</label>
-                        <input type="email" name="email" class="form-control" required="required">
+                        <label>Hora de Inicio *</label>
+                        <input type="text" name="startHour" class="form-control" required="required" placeholder="HH:MM am">
                     </div>
                     <div class="form-group">
-                        <label>Phone</label>
-                        <input type="number" class="form-control">
+                        <label>Seleccione el Paseador *</label>
+                        <input type="text" name="rider" class="form-control" required="required" placeholder="Paseador">
                     </div>
                     <div class="form-group">
-                        <label>Company Name</label>
-                        <input type="text" class="form-control">
+                        <label>Duración del Paseo (Minutos) *</label>
+                        <input type="text" name="duration" class="form-control" required="required" placeholder="Máximo 120">
                     </div>
                 </div>
                 <div class="col-sm-5">
                     <div class="form-group">
-                        <label>Subject *</label>
-                        <input type="text" name="subject" class="form-control" required="required">
+                        <label>Dirección *</label>
+                        <input type="text" name="addres" class="form-control" required="required">
                     </div>
                     <div class="form-group">
-                        <label>Message *</label>
-                        <textarea name="message" id="message" required="required" class="form-control" rows="8"></textarea>
+                        <label>Inidicaciones Extra</label>
+                        <textarea name="extraHints" id="extraHints" class="form-control" rows="8"></textarea>
                     </div>
                     <div class="form-group">
-                        <button type="submit" name="submit" class="btn btn-primary btn-lg" required="required">Submit message</button>
+                        <button type="submit" name="submit" class="btn btn-primary btn-lg" required="required">Solicitar</button>
                     </div>
                 </div>
             </form>
