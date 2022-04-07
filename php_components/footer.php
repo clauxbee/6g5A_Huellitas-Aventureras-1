5468f6b1bd4d858e022a3cf4c654518c5ab9d35a
Braulio-Palagot <bdpalagot@gmail.com>
2022-04-07 14:59:27 -0500

    Update footer.php
    Cambios Realizados:
        1) Se actualizó la vista del footer para adecuarse a la aplicación en desarrollo.
    
    Cambios Pendientes:
        1) Cambiar las fotos de la galería por las que se incluyan en las entradas del Blog.

--- php_components/footer.php
+++ php_components/footer.php
@@ -1,44 +1,32 @@
 <footer>
     <div class="container">
-        <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
-            <h4>About Us</h4>
-            <p>Day is tellus ac cursus commodo, mauesris condime ntum nibh, ut fermentum mas justo sitters.</p>
+        <div class="col-md-6 fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+            <h4>Sobre Nosotros</h4>
+            <p>Queremos lo mejor para tu peludito, es por ello que en "Huellitas aventureras" nos empeñamos todos los
+                días en hacerlo sentir bien y cuidarlos como si fueran nuestros. Piensa en tu peludito y regalale un
+                paseo con los mejores paseadores de la ciudad, forma parte de nuestra gran familia.</p>
             <div class="contact-info">
                 <ul>
-                    <li><i class="fa fa-home fa"></i>Suite 54 Elizebth Street, Victoria State Newyork, USA </li>
-                    <li><i class="fa fa-phone fa"></i> +38 000 129900</li>
-                    <li><i class="fa fa-envelope fa"></i> info@domain.net</li>
+                    <li><i class="fa fa-phone fa"></i> +52 272 315 1862</li>
+                    <li><i class="fa fa-envelope fa"></i> huellitas-aventureras@gmail.com</li>
                 </ul>
             </div>
         </div>
 
-        <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+        <div class="col-md-6 fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
             <div class="text-center">
-                <h4>Photo Gallery</h4>
+                <h4>Blog</h4>
                 <ul class="sidebar-gallery">
-                    <li><a href="#"><img src="img/gallery1.png" alt="" /></a></li>
-                    <li><a href="#"><img src="img/gallery2.png" alt="" /></a></li>
-                    <li><a href="#"><img src="img/gallery3.png" alt="" /></a></li>
-                    <li><a href="#"><img src="img/gallery4.png" alt="" /></a></li>
-                    <li><a href="#"><img src="img/gallery5.png" alt="" /></a></li>
-                    <li><a href="#"><img src="img/gallery6.png" alt="" /></a></li>
+                    <li><a href="#"><img src="img/gallery1.png" alt="Entrada 1"/></a></li>
+                    <li><a href="#"><img src="img/gallery1.png" alt="Entrada 2"/></a></li>
+                    <li><a href="#"><img src="img/gallery1.png" alt="Entrada 3"/></a></li>
+                    <li><a href="#"><img src="img/gallery1.png" alt="Entrada 4"/></a></li>
+                    <li><a href="#"><img src="img/gallery1.png" alt="Entrada 5"/></a></li>
+                    <li><a href="#"><img src="img/gallery1.png" alt="Entrada 6"/></a></li>
+                    <li><a href="#"><img src="img/gallery1.png" alt="Entrada 7"/></a></li>
+                    <li><a href="#"><img src="img/gallery1.png" alt="Entrada 8"/></a></li>
                 </ul>
             </div>
-        </div>
-
-        <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="900ms">
-            <div class="">
-                <h4>Newsletter Registration</h4>
-                <p>Subscribe today to receive the latest Day news via email. You may unsubscribe from this service at any time</p>
-                <div class="btn-gamp">
-                    <input type="email" class="form-control" id="exampleInputEmail3" placeholder="Enter Email">
-                </div>
-                <div class="btn-gamp">
-                    <a type="submit" class="btn btn-default">Subscribe</a>
-                </div>
-
-            </div>
-
         </div>
 </footer>
 
@@ -49,16 +37,15 @@
                 <ul class="social-network">
                     <li><a href="#" class="fb tool-tip" title="Facebook"><i class="fa fa-facebook"></i></a></li>
                     <li><a href="#" class="twitter tool-tip" title="Twitter"><i class="fa fa-twitter"></i></a></li>
-                    <li><a href="#" class="gplus tool-tip" title="Google Plus"><i class="fa fa-google-plus"></i></a></li>
-                    <li><a href="#" class="linkedin tool-tip" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
-                    <li><a href="#" class="ytube tool-tip" title="You Tube"><i class="fa fa-youtube-play"></i></a></li>
                 </ul>
             </div>
         </div>
 
         <div class="col-md-4 col-md-offset-4">
             <div class="copyright">
-                &copy; Day 2015 by <a target="_blank" href="http://bootstraptaste.com/" title="Free Twitter Bootstrap WordPress Themes and HTML templates">Bootstrap Themes</a>.All Rights Reserved.
+                &copy; Day 2015 by <a target="_blank" href="http://bootstraptaste.com/"
+                                      title="Free Twitter Bootstrap WordPress Themes and HTML templates">Bootstrap
+                    Themes</a>.All Rights Reserved.
             </div>
             <!--
                 All links in the footer should remain intact.
