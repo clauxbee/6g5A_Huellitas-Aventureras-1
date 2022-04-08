11fad950d7147b3f5108040f15f2d71f68a77d0e
Braulio-Palagot <bdpalagot@gmail.com>
2022-04-08 10:55:23 -0500

    UPDATE blog_bestMonth.php y blog_bestMonth_mark.php
    Cambios Realizados:
        1) Se movió la propiedad de background-image para no afectar a más de un componente.

--- php_components/blog_bestMonth.php
+++ php_components/blog_bestMonth.php
@@ -4,6 +4,6 @@
             <h4>Los peluditos también sienten!</h4>
             <p><em>Las mascotas son sensibles al ambiente en donde habitan, por lo cual debes de saber ciertas cosas que le pueden provocar un malestar.</em><br><br>En este apartado de la página encontrarás ciertas entradas con datos curiosos que te podrían ser de utilidad para entender a tu peludo.</p>
         </div>
-        <div class="right-image hidden-xs"></div>
+        <div class="right-image hidden-xs" style="background-image: url(../img/blog1.jpg);"></div>
     </div>
 </section>
--- php_components/blog_bestMonth_mark.php
+++ php_components/blog_bestMonth_mark.php
@@ -5,6 +5,6 @@
             <p>Solo por esta semana, al realizar tu primer paseo pagaras solo el 50% del precio original. Nos importa tu mascota por ello buscamos la forma de apoyarte, ¡No dejes pasar esta oferta!.</p>
         <p>Puedes saber más de esta oferta y otras promociones en la seccion de abajo.</p>
         </div>
-        <div class="right-image hidden-xs"></div>
+        <div class="right-image hidden-xs" style="background-image: url(../img/50porciento.png);"></div>
     </div>
 </section>
