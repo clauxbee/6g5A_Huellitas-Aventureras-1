a526c36cf6e17ca1721adbc3c3c3fdcf632519a8
AnthonRM <AntonioRM270601@gmail.com>
2022-04-08 10:17:17 -0500

    Pantalla de Marketing_AntonioRM

---
+++ php_components/blog_bestMonth_mark.php
@@ -0,0 +1,10 @@
+<section class="action">
+    <div class="container">
+        <div class="left-text hidden-xs">
+            <h4>Tu primer paseo al 50% de descuento.</h4>
+            <p>Solo por esta semana, al realizar tu primer paseo pagaras solo el 50% del precio original. Nos importa tu mascota por ello buscamos la forma de apoyarte, ¡No dejes pasar esta oferta!.</p>
+        <p>Puedes saber más de esta oferta y otras promociones en la seccion de abajo.</p>
+        </div>
+        <div class="right-image hidden-xs"></div>
+    </div>
+</section>
