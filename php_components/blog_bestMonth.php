dde7cefa7ac7270f86c1985ac2cbd631ecb270de
JocelynFL <jocelyn.jfl@gmail.com>
2022-04-08 10:38:46 -0500

    Pantalla blog

--- php_components/blog_bestMonth.php
+++ php_components/blog_bestMonth.php
@@ -1,8 +1,8 @@
 <section class="action">
     <div class="container">
         <div class="left-text hidden-xs">
-            <h4>Amet porro numquam ratione</h4>
-            <p><em>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi ut explicabo magni sapiente.</em><br><br>Inventore at quia, vel in repellendus, cumque dolorem autem ad quidem mollitia porro blanditiis atque rerum debitis eveniet nostrum aliquam. Sint aperiam expedita sapiente amet officia quis perspiciatis adipisci, iure dolorem esse exercitationem!</p>
+            <h4>Los peluditos también sienten!</h4>
+            <p><em>Las mascotas son sensibles al ambiente en donde habitan, por lo cual debes de saber ciertas cosas que le pueden provocar un malestar.</em><br><br>En este apartado de la página encontrarás ciertas entradas con datos curiosos que te podrían ser de utilidad para entender a tu peludo.</p>
         </div>
         <div class="right-image hidden-xs"></div>
     </div>
