13da2d8ffe58e147cd1549529dff4d7f8c2ca8cf
clauxbee <clauxgrl@gmail.com>
2022-04-27 14:15:55 -0500

    Pantalla cuenta, formulario de registro

--- php_components/register_form.php
+++ php_components/register_form.php
@@ -1,77 +1,63 @@
-<div class="main">
-    <div class="shop_top">
-        <div class="container">
-            <div class="col-md-6">
-                <div class="resgister_form-page">
-                <br>
+<section class="contact-page">
+    <div class="container">
+        <div class="text-center">
+        <h2 class="title">Crea tu cuenta</h2>
+             
+        </div>
+        <div class="row contact-wrap">
+            <div class="status alert alert-success" style="display: none"></div>
+            <form id="main-contact-form" class="contact-form" name="contact-form" method="post" action="sendemail.php">
+                <div class="col-sm-5 col-sm-offset-1">
                 <img src = img/regIcon.png>
-                    <h4 class="title">Crea tu cuenta</h4>
-                    <p>Ingresa los siguientes datos</p>
-                   
-                    <br>
-                    <div id="registerbox" class="registerbox">
-                        <form action="" method="post" name="register" id="register-form">
-                            <fieldset class="input">
-                            <p id="register-form-username">
-                                    <label for="modrgstr_username">Nombre</label>
-                                    <input id="modrgstr_username" type="text" name="nombreUser" class="inputbox" size="18" autocomplete="off">
-                                </p>
-                                <p id="register-form-userage">
-                                    <label for="modrgstr_userage">Edad</label>
-                                    <input id="modrgstr_userare" type="text" name="ageUser" class="inputbox" size="18" autocomplete="off">
-                                </p>
-                                
-                           </div>                              
-                           <div class="clear"></div>
+                <p>      Ingresa los siguientes datos</p>
+                    <div class="form-group">
+                                <label for="modrgstr_username">Nombre</label>
+                                <input id="modrgstr_username" type="text" name="nombreUser" class="inputbox" size="18" autocomplete="off">
+                        </div>
+                    <div class="form-group">
+                                 <label for="modrgstr_userage">Edad</label>
+                                 <input id="modrgstr_userare" type="text" name="ageUser" class="inputbox" size="18" autocomplete="off">
+                         </div>
                 </div>
-            </div>
-            <div class="col-md-6">
-                <div class="register-title">
-                   
-                    <div id="registerbox" class="registrerbox">
-                        <form action="" method="post" name="login" id="register-form">
-                            <fieldset class="input">
-<br><br><br>
-                            <p id="register-form-useradress">
-                                    <label for="modrgst_useradress">Dirección</label>
-                                    <input id="modrgstr_useradress" type="text" name="adressUser" class="inputbox" size="18" autocomplete="off">
-                                </p>
-                                
-                                <p id="register-form-userphone">
-                                    <label for="modrgst_userphone">Número de celular</label>
-                                    <input id="modrgstr_userphone" type="text" name="phoneUser" class="inputbox" size="18" autocomplete="off">
-                                </p>
-
-                                <p>
-                                    <label>Género</label>
+                <div class="col-sm-5">
+                    <div class="form-group">
+                        <br>
+                        <label>Dirección </label>
+                        <input id="modrgstr_useradress" type="text" name="adressUser" class="inputbox" size="18" autocomplete="off">
+                    </div>
+                    <div class="form-group">
+                            <label for="modrgst_userphone">Número de celular</label>
+                            <input id="modrgstr_userphone" type="text" name="phoneUser" class="inputbox" size="18" autocomplete="off">
+                    </div>
+                    <div class="form-group">
+                        <p>
+                                 <label>Género</label>
                                 <input name="Genero" type="radio" id="Genero" value="Masculino"> Masculino</input>
                                 <input name="Genero" type="radio" id="Genero" value="Femenino"> Femenino</input>
                                 <input name="Genero" type="radio" id="Genero" value="Otro"> Otro</input>
-                           </p>    
-                                <p id="login-form-username">
-                                    <label for="modrgstr_useremail">Correo</label>
-                                    <input id="modrgstr_useremail" type="text" name="email" class="inputbox" size="18" autocomplete="off">
-                                </p>
-                                <p id="register-form-password">
-                                    <label for="modrgstr_passwd">Contraseña</label>
+                        </p>
+                  </div>
+                    <div class="form-group">
+                                <label for="modrgstr_useremail">Correo</label>
+                                <input id="modrgstr_useremail" type="text" name="email" class="inputbox" size="18" autocomplete="off">
+                   </div>
+
+                    <div class="form-group">
+                                     <label for="modrgstr_passwd">Contraseña</label>
                                     <input id="modrgstr_passwd" type="password" name="password" class="inputbox" size="18" autocomplete="off">
-                                </p>
-                                <p id="register-form-password">
+                    </div>
+
+                    <div class="form-group">
                                     <label for="modrgstr_passwd">Repetir contraseña</label>
                                     <input id="modrgstr_passwd" type="password" name="password" class="inputbox" size="18" autocomplete="off">
-                                </p>
-                               <br><br><br><b></b>
-                                    
-                                    <button href="inicio.html" type="getnow" name="subscribe" class="btn btn-primary btn-lg" required="required">Registarme</button>
-                                    <br><br><br><br>
-                               
-                                </div>
-                            </fieldset>
-                        </form>
                     </div>
+
+                    <div class="form-group">
+                        <button type="submit" name="submit" class="btn btn-primary btn-lg" required="required">Registrarse</button>
+                    </div>
+
                 </div>
-                <div class="clear"></div>
-            </div>
-        </div>
-    </div>
-</div>
+            </form>
+        </div><!--/.row-->
+    </div><!--/.container-->
+</section><!--/#contact-page-->
