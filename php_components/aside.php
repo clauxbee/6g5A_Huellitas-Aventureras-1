6f00787c5ca0edc30f44ad1137401da2f4bb73ee
clauxbee <clauxgrl@gmail.com>
2022-05-03 15:59:18 -0500

    Aside en solicitar servicio

--- php_components/aside.php
+++ php_components/aside.php
@@ -1,11 +1,24 @@
-<aside>
-        <div class="social-icon">
-                <ul class="social-network">
-                    <li><a href="#" class="fb tool-tip" title="Facebook"><i class="fa fa-facebook"></i></a></li>
-                    <li><a href="#" class="twitter tool-tip" title="Twitter"><i class="fa fa-twitter"></i></a></li>
-                </ul>
-        </div>
-            <div class="copyright">
-                    &copy;   2022 Huellitas Aventureras.
-            </div>
- </aside>
+      
+ <aside style="
+display: inline-block;
+width: 150px;
+height: 470px;
+background-color: #65AAF0;
+float: right;
+margin-top: 30px;
+"> 
+<center>
+        <p style="color: white; margin-top: 100px;">Siguenos en nuestras redes.</p>
+                        <div class="social-icon">
+                               
+                                <ul class="social-network">
+                                 <li><a href="#" class="fb tool-tip" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                                 <li><a href="#" class="twitter tool-tip" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+                                </ul>
+                         </div>
+                                <div class="copyright">
+                                   &copy; 2022 Huellitas Aventureras.
+                               </div>
+                            
+        </aside>
+        </center>
